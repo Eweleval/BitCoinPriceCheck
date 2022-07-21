7fc8422598ca796a7cca77308da8f55bf2355997
WellzVal <valeweleokenyi@gmail.com>
2022-07-21 13:49:51 +0100

    Initial Commit

---
+++ BitCoinPriceCheck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitCoinPriceCheck
+//
+//  Created by Decagon on 21/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
